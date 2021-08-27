c097ca97cbfec078e1410d2d1acbd8a9c04e76ce
Muhammad Razif Rizqullah <razifrizqullah@gmail.com>
2021-08-27 09:50:15 +0700

    Add project files.

---
+++ EAP_Event_Based_Asynchronous_Pattern/BWSimple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace EAP_Event_Based_Asynchronous_Pattern {
+  class BWSimple {
+    private BackgroundWorker _bw;
+    internal BWSimple() {
+      _bw = new BackgroundWorker();
+      _bw.DoWork += Foo;
+      _bw.RunWorkerAsync("Heyyyaa");
+      Console.ReadKey();
+    }
+    private void Foo(object sender, DoWorkEventArgs e) {
+      Console.WriteLine(e.Argument);
+    }
+  }
+}
---
+++ EAP_Event_Based_Asynchronous_Pattern/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EAP_Event_Based_Asynchronous_Pattern {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("EAP!");
+      BWSimple bw = new BWSimple();
+    }
+  }
+}
